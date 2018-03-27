847630a616fe6916b0d4d23a3ac796ed522c1d54
khlayil <med.khalil.aloui.1994@gmail.com>
2018-03-27 22:33:52 +0100

    forum rectification,icon login rectification

--- src/GestionUserBundle/Controller/DefaultController.php
+++ src/GestionUserBundle/Controller/DefaultController.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace GestionUserBundle\Controller;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\ColumnChart;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\Histogram;
 use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
+use ForumBundle\Entity\Post;
+use ForumBundle\Entity\Rubrique;
 use GestionUserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -63,13 +67,13 @@ class DefaultController extends Controller
         $pieChart = new PieChart();
         $pieChart->getData()->setArrayToDataTable(
             [['roles', 'number role '],
-                ['ROLE_ADMIN',     $a],
-                ['ROLE_USER',      $us],
-                ['ROLE_VETE',  $v]
+                ['admin',     $a],
+                ['client',      $us],
+                ['veterinary',  $v]
 
             ]
         );
-        $pieChart->getOptions()->setTitle('My Daily Activities');
+        $pieChart->getOptions()->setTitle('registred user type');
         $pieChart->getOptions()->setHeight(500);
         $pieChart->getOptions()->setWidth(900);
         $pieChart->getOptions()->getTitleTextStyle()->setBold(true);
@@ -79,8 +83,67 @@ class DefaultController extends Controller
         $pieChart->getOptions()->getTitleTextStyle()->setFontSize(20);
 
 
+        $chien=$em->getRepository(Post::class)->countChienPosts();
+        $chat=$em->getRepository(Post::class)->countChatPosts();
+        $oiseau=$em->getRepository(Post::class)->countOiseauPosts();
+        $rongeur=$em->getRepository(Post::class)->countRongeurPosts();
 
-        return $this->render('@GestionUser/admin/index.html.twig', array('piechart' => $pieChart));
+
+
+
+
+        $pieChart1 = new PieChart();
+        $pieChart1->getData()->setArrayToDataTable(
+            [['category', 'post number '],
+                ['chat',   (int)  $chien],
+                ['chien',   (int)   $chat],
+                ['rongeur',(int)  $oiseau],
+                ['oiseaux', (int) $rongeur]
+
+            ]
+        );
+        $pieChart1->getOptions()->setTitle('nombre post rapport category');
+        $pieChart1->getOptions()->setHeight(500);
+        $pieChart1->getOptions()->setWidth(900);
+        $pieChart1->getOptions()->getTitleTextStyle()->setBold(true);
+        $pieChart1->getOptions()->getTitleTextStyle()->setColor('#009900');
+        $pieChart1->getOptions()->getTitleTextStyle()->setItalic(true);
+        $pieChart1->getOptions()->getTitleTextStyle()->setFontName('Arial');
+        $pieChart1->getOptions()->getTitleTextStyle()->setFontSize(20);
+
+
+        $chienp=$em->getRepository(Post::class)->countCommentChienPosts();
+        $chatp=$em->getRepository(Post::class)->countCommentChatPosts();
+        $oiseaup=$em->getRepository(Post::class)->countCommentChatPosts();
+        $rongeurp=$em->getRepository(Post::class)->countCommentChatPosts();
+
+
+        $col = new ColumnChart();
+        $col->getData()->setArrayToDataTable(
+            [
+                ['Time of Day', 'nombre comment', ['role' => 'annotation'], 'nombre post', ['role' => 'annotation']],
+                [['v' => "aa", 'f' => 'chien '],  (int)$chienp, 'comment', (int) $chien, 'post'],
+                [['v' => "bb", 'f' => 'chat'],  (int)$chatp, 'comment',  (int)$chat, 'post'],
+                [['v' => "daz", 'f' => 'oiseaux '], (int)$oiseaup, 'comment',    (int)$oiseau,  'post'],
+                [['v' => "aed", 'f' => 'rongeur '], (int)$rongeurp, 'comment', (int)$rongeur,  'post'],
+            ]
+        );
+        $col->getOptions()->setTitle('rapport comment et post pour chaque category');
+        $col->getOptions()->getAnnotations()->setAlwaysOutside(true);
+        $col->getOptions()->getAnnotations()->getTextStyle()->setFontSize(14);
+        $col->getOptions()->getAnnotations()->getTextStyle()->setColor('#000');
+        $col->getOptions()->getAnnotations()->getTextStyle()->setAuraColor('none');
+        $col->getOptions()->getHAxis()->setTitle('Time of Day');
+
+        $col->getOptions()->getHAxis()->getViewWindow()->setMin([5, 30, 0]);
+        $col->getOptions()->getHAxis()->getViewWindow()->setMax([17, 30, 0]);
+        $col->getOptions()->getVAxis()->setTitle('Rating (scale of 1-10)');
+        $col->getOptions()->setWidth(900);
+        $col->getOptions()->setHeight(500);
+
+
+
+        return $this->render('@GestionUser/admin/index.html.twig', array('piechart' => $pieChart,"piechart1"=>$pieChart1,"cole"=>$col));
 
     }
 
--- src/GestionUserBundle/Controller/ProfileController.php
+++ src/GestionUserBundle/Controller/ProfileController.php
@@ -26,6 +26,7 @@ class ProfileController extends BaseController
     public function showAction()
     {
         $user = $this->getUser();
+
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
